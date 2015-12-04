707e05832ba7bee9f2f7a53498a29f89283de881
jvvlives2005 <jvvlives2005@hotmail.com>
2015-12-03 20:03:21 -0500

    initial commit for 12-03

---
+++ src/nyc/c4q/jvvlives2005/Queue.java
@@ -0,0 +1,20 @@
+package nyc.c4q.jvvlives2005;
+/**
+ * Created by c4q-joshelynvivas on 12/3/15.
+ *
+ * Implement a Queue class twice:
+ Using a Linked List
+ Using a Hash Map.
+ Do an analysis of the time and space complexity of each solution and support a decision for one over the other.
+ *
+ */
+public class Queue
+{
+    //a method with Linked List
+
+    //a method with HashMap
+}
+
+
+
+//not sure of their runtimes though (maybe O(N))
